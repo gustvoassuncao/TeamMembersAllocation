a0dc880259efee7bd96555b90b1e89ab33e7cafc
Gustavo Monteiro de Assunção <monteirocontacto@gmail.com>
2022-09-14 08:08:08 -0300

    adjustment of propdrilling component app to employees

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,130 @@ import "./App.css";
 import Header from "./Header";
 import Employees from "./Employees";
 import Footer from "./Footer";
+import { useState } from "react";
 
 export default function App() {
+
+  const [selectedTeam, setTeam] = useState("TeamB");
+
+  const [employees, setEmployees] = useState([{
+    id: 1,
+    fullName: "Bob Jones",
+    designation: "JavaScript Developer",
+    gender: "male",
+    teamName: "TeamA",
+    img: Bob
+  },
+  {
+    id: 2,
+    fullName: "Jill Bailey",
+    designation: "Node Developer",
+    gender: "female",
+    teamName: "TeamA",
+    img: Jill
+  },
+  {
+    id: 3,
+    fullName: "Gail Shepherd",
+    designation: "Java Developer",
+    gender: "female",
+    teamName: "TeamA",
+    img: Gail
+  },
+  {
+    id: 4,
+    fullName: "Sam Reynolds",
+    designation: "React Developer",
+    gender: "male",
+    teamName: "TeamB",
+    img: Sam
+  },
+  {
+    id: 5,
+    fullName: "David Henry",
+    designation: "DotNet Developer",
+    gender: "male",
+    teamName: "TeamB",
+    img: David
+  },
+  {
+    id: 6,
+    fullName: "Sarah Blake",
+    designation: "SQL Server DBA",
+    gender: "female",
+    teamName: "TeamB",
+    img: Sarah
+  },
+  {
+    id: 7,
+    fullName: "James Bennet",
+    designation: "Angular Developer",
+    gender: "male",
+    teamName: "TeamC",
+    img: James
+  },
+  {
+    id: 8,
+    fullName: "Jessica Faye",
+    designation: "API Developer",
+    gender: "female",
+    teamName: "TeamC",
+    img: Jessica
+  },
+  {
+    id: 9,
+    fullName: "Lita Stone",
+    designation: "C++ Developer",
+    gender: "female",
+    teamName: "TeamC",
+    img: Lita
+  },
+  {
+    id: 10,
+    fullName: "Daniel Young",
+    designation: "Python Developer",
+    gender: "male",
+    teamName: "TeamD",
+    img: Daniel
+  },
+  {
+    id: 11,
+    fullName: "Adrian Jacobs",
+    designation: "Vue Developer",
+    gender: "male",
+    teamName: "TeamD",
+    img: Adrian
+  },
+  {
+    id: 12,
+    fullName: "Devin Monroe",
+    designation: "Graphic Designer",
+    gender: "male",
+    teamName: "TeamD",
+    img: Devin
+  }]);
+
+  function handleTeamSelectionChange(event){
+    setTeam(event.target.value);
+  }
+
+  function handleEmployeeCardClick(event){
+    const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTarget.id)
+      ? employee.teamName === selectedTeam ? { ...employee, teamName: '' }
+        : { ...employee, teamName: selectedTeam } : employee);
+    setEmployees(transformedEmployees);
+  }
+
   return (
     <div>
       <Header />
-      <Employees />
+      <Employees 
+      employees={employees}
+      selectedTeam={selectedTeam} 
+      handleTeamSelectionChange={handleTeamSelectionChange}
+      handleEmployeeCardClick={handleEmployeeCardClick}
+      
+      />
       <Footer />
     </div>
   )
